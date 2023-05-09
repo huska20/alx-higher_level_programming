c085149dd78659a69bc96a1ab0e79100cfb21d78
huska20 <sharifka87@gmail.com>
2023-05-09 08:05:59 -0700

    modified and updated portion of code in the program

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# 12-fizzbuzz.py
+
+
+def fizzbuzz():
+    """Print the numbers from 1 to 100 separated by a space.
+
+    For multiples of three,"""
+    for number in range(1, 101):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz ", end="")
+        elif number % 3 == 0:
+            print("Fizz ", end="")
+        elif number % 5 == 0:
+            print("Buzz ", end="")
+        else:
+            print("{} ".format(number), end="")
