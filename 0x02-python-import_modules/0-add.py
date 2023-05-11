4718a11bc437d134efd806550348b507261af6d5
huska20 <sharifka87@gmail.com>
2023-05-11 11:49:36 -0700

    updated the first imported file in python after few error

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
-from add_0 import add
+if __name__ == "__main__":
+    """Printing sum  one and two."""
+    from add_0 import add
 
-a = 1
-b = 2
-
-result = add(a, b)
-
-print("{0:d} + {1:d} = {2:d}\n".format(a, b, result))
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
