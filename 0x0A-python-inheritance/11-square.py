e61c49338c5a4f3f6852104e5ba202fbaff25fa3
huska20 <sharifka87@gmail.com>
2023-06-12 12:23:10 -0700

    upated files

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -3,9 +3,11 @@
 Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
+    """Represent a square."""
 
     def __init__(self, size):
         """Initialize a new square.
+
         Args:
             size (int): The size of the new square.
         """
